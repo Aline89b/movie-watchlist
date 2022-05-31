02a2e3b6a6fd49d8cb172989f4f3f5fea3e06285
Aline89b <95961350+Aline89b@users.noreply.github.com>
2022-05-31 12:08:38 +0200

    "updated files"

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const searchTerm = document.querySelector(".searchTerm")
 const movieList = document.getElementById("display-movie-list")
 const searchButton = document.querySelector(".searchButton")
 let searchInput
-let movieId = []
+let movies = []
 
 
 
@@ -18,102 +18,50 @@ movieList.innerHTML =
 searchTerm.addEventListener("keyup", (e) => {
    searchInput = e.target.value.toLowerCase()
   console.log(searchInput)
-/*  const filteredMovies = movieSearch.filter(movie => {
+  /*filteredMovies = movieSearch.filter(movie => {
   return movie.Title.toString().toLowerCase().includes(searchInput)
   })
   console.log(filteredMovies)*/
 })
 
-//searchButton.addEventListener("click", getMovies)
+searchButton.addEventListener("click", getTitle)
 
-
-//function getMovies() {
 
   async function getTitle() {
-
-    const urlMovie = await fetch(`http://www.omdbapi.com/?s=${searchInput}&apikey=ea2329ec`)
-    const urlID = await fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=ea2329ec`)
-
-      Promise.all([urlMovie, urlID]).then(values => {
-        return Promise.all(values.map(r => r.json()))
-      }).then(([urlMovie,urlID]) => {
-        console.log(urlMovie)
-        console.log(urlID)
-      }).catch(e => {
-        console.log("nope")
-        console.log(e)
+      movieList.innerHTML =""
+    let data = await fetch(`http://www.omdbapi.com/?s=${searchInput}&apikey=ea2329ec`)
+      .then(res=> res.json())
+      .then(data => {
+        movies = data.Search
+        console.log(movies)
+        return movies
       })
 
-    }
-  //}
 
-  /*  try {
-      const url = `http://www.omdbapi.com/?s=${searchInput}&apikey=ea2329ec`
-      const res = await fetch(url)
-      const data  = await res.json()
-      return data
+      movies.map(movie => fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=ea2329ec`)
+        .then(r => r.json())
+        .then(movie => {
+          console.log(movie)
+          movieList.innerHTML += `
+        <div class = "movies">
+           <div class="movie-card poster"><img src= "${movie.Poster}"></div>
+           <div class="movie-card title"> ${movie.Title} </div>
+           <div class="movie-card score"><img src="imgs/star-score.png"> ${movie.imdbRating} </div>
+           <div class="movie-card runtime"> ${movie.Runtime} </div>
+           <div class="movie-card genre"> ${movie.Genre} </div>
+           <div class="movie-card watchlist-btn"><a href= "javascript: getBusy()"><img src="imgs/watchlist-btn.png"></a> watchlist </div>
+           <div class="movie-card plot"> ${movie.Plot} </div>
+        </div>
+             <hr>
 
-    } catch(err) {
-      console.error(err)
-      alert(err)
-    }
-  }
+         `
 
-getTitle().then(data => {
-  console.log(data.Search)
-  movieSearch =  data.Search
-  movieId = movieSearch.map(movie => {
+       }))
 
-  /*  movieList.innerHTML +=
-    `
-  <div class="movies">
-   <div class="poster"><img src= "${movie.Poster}"></div>
-   <div class="title"> ${movie.Title} </div>
-   <div class="runtime"> ${movie.Year} </div>
-  </div>
-    <hr>
-   `
-   console.log(movie.imdbID)
-   return movie.imdbID
+      document.querySelector(".initialScreen").style.display = "none"
 
-   })
-      console.log(movieId)
-    })
+      }
 
-
-}
-
-   async function getMovieId() {
-
-    try {
-      const url = `http://www.omdbapi.com/?i=${movie.imdbID}&apikey=ea2329ec`
-      const res = await fetch(url)
-      const data = await res.json()
-      console.log(data)
-      return data
-
-  } catch(err) {
-    console.error(err)
-    alert(err)
-     }
-
-
-
-
-  getMMovieId().then(data => {
-     movieList.innerHTML += `
-   <div class="movies">
-    <div class="poster"><img src= "${data.Poster}"></div>
-    <div class="title"> ${data.Title} </div>
-    <div class="score"><img src="star-score.png"> ${data.imdbRating} </div>
-    <div class="runtime"> ${data.Runtime} </div>
-    <div class="genre"> ${data.Genre} </div>
-    <div class="watchlist-btn"><a href="#"><img src="watchlist-btn.png"></a> watchlist </div>
-    <div class="plot"> ${data.Plot} </div>
-  </div>
-        <hr>
-    `
-    })
- document.querySelector(".initialScreen").style.display = "none"
- }
-*/
+      function getBusy(){
+        alert("get busy")
+      }
