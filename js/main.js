ed53ea4ef7bbaccb009650069903d0f421d71aa8
Aline89b <95961350+Aline89b@users.noreply.github.com>
2022-06-17 10:06:50 +0200

    added remove icon and enter keypress event

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ const searchTerm = document.querySelector(".searchTerm")
 const movieList = document.getElementById("display-movie-list")
 const searchButton = document.querySelector(".searchButton")
 let searchInput
-let favMovies = []
-let movie = {}
-
-
+let movies = []
+console.log(movies)
+let favMovies = JSON.parse(localStorage.getItem('favMovies')) || [];
+let movieID
 
 movieList.innerHTML =
 
@@ -17,55 +17,73 @@ movieList.innerHTML =
   `
 
 searchTerm.addEventListener("keyup", (e) => {
-  e.preventDefault()
-   searchInput = e.target.value.toLowerCase()
+
+    searchInput = e.target.value.toLowerCase()
   console.log(searchInput)
-  /*filteredMovies = movieSearch.filter(movie => {
-  return movie.Title.toString().toLowerCase().includes(searchInput)
-  })
-  console.log(filteredMovies)*/
 })
 
+searchTerm.addEventListener("keypress", (e) => {
+  if(e.key === "Enter") {
+    e.preventDefault()
+    searchButton.click()
+  }
+})
 searchButton.addEventListener("click", getTitle)
 
 
   async function getTitle() {
-      movieList.innerHTML =""
+        movieList.innerHTML =""
     let data = await fetch(`http://www.omdbapi.com/?s=${searchInput}&apikey=ea2329ec`)
       .then(res=> res.json())
       .then(data => {
         movies = data.Search
         console.log(movies)
+
         return movies
+
       })
 
+      getMovieCard()
 
-      movies.map(movie => fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=ea2329ec`)
+      }
+
+      function getMovieCard() {
+         movies.map(movie => fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=ea2329ec`)
         .then(r => r.json())
         .then(movie => {
           console.log(movie)
-          movieList.innerHTML += `
+
+           movieList.innerHTML += `
         <div class = "movies">
            <div class="movie-card poster"><img src= "${movie.Poster}"></div>
            <div class="movie-card title"> ${movie.Title} </div>
            <div class="movie-card score"><img src="imgs/star-score.png"> ${movie.imdbRating} </div>
            <div class="movie-card runtime"> ${movie.Runtime} </div>
            <div class="movie-card genre"> ${movie.Genre} </div>
-           <div class="movie-card watchlist-btn"><a href= "javascript: getWatchlist(movie)"><img src="imgs/watchlist-btn.png"></a> watchlist </div>
+           <div class="movie-card watchlist-btn"><a id="link" href="#" onclick ="javascript: getWatchlist(movieID); return false;"><img src="imgs/watchlist-btn.png"></a> watchlist </div>
            <div class="movie-card plot"> ${movie.Plot} </div>
         </div>
              <hr>
 
          `
-        }))
-
-      document.querySelector(".initialScreen").style.display = "none"
+         movieID = movie.imdbID
+         console.log(movieID)
+         return movieID
+       }))
+        //  document.querySelector(".initialScreen").style.display = "none"
 
       }
 
-      function getWatchlist(movie) {
-        console.log( `${movie.imdbID}`)
-        console.log(`${movie}`)
-        favMovies.push(`${movie}`)
-        console.log(localStorage.setItem("favMovies", JSON.stringify(favMovies)))
+    //  document.getElementById("link").addEventListener("click", getWatchlist)
+
+
+      function getWatchlist(movieID) {
+                console.log(movieID)
+                fetch(`http://www.omdbapi.com/?i=${movieID}&apikey=ea2329ec`)
+                .then(r=> r.json())
+                .then(data => {
+                  favMovies.push(data)
+                  console.log(data)
+                  localStorage.setItem("favMovies", JSON.stringify(favMovies))
+          })
         }
